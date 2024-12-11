6c17bb3359e345f22d1ba2a2c8b42ac0eab99779
samyleedev <samy.levy.dev@gmail.com>
2024-12-11 20:28:49 +0100

    fix to make init app.e2n passed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '../user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
-import { UserService } from 'src/user/user.service';
+import { UserService } from '../user/user.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
